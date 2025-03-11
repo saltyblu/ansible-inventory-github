f37302e335d8f5c033ae9b7b2b52ec5b1a081f43
Salty Blu <v.schmitz01@gmail.com>
2025-03-11 15:16:30 +0100

    get first group if found

--- plugins/inventory/github_repositories_inventory.py
+++ plugins/inventory/github_repositories_inventory.py
@@ -101,8 +101,9 @@ class InventoryModule(BaseInventoryPlugin, Cacheable):
             matches = re.findall(regex_filter, repository['name'])
             if matches:
                 result = []
-                main_group = f'main-{matches[0]}'
-                result.append(main_group)
+                main_group = f'main-{matches[0][0]}'
+                if main_group:
+                    result.append(main_group)
                 for match in matches:
                     result.extend(match)
                 return result
